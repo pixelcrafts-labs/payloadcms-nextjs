57dd501c9cd1f2b4193b6fc7a2bf49c037df8a1b
vuquangpham <vuquangpham2909@gmail.com>
2024-06-27 20:31:46 +0700

    feat: add libs functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,22 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
+
+export function debounce<F extends (...args: Parameters<F>) => ReturnType<F>>(
+	func: F,
+	waitFor: number = 200
+): (...args: Parameters<F>) => void {
+	let timeout: NodeJS.Timeout;
+	return (...args: Parameters<F>): void => {
+		clearTimeout(timeout);
+		timeout = setTimeout(() => func(...args), waitFor);
+	};
+}
+
+export function timeoutForPromise(timeout: number): Promise<void> {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve();
+		}, timeout);
+	});
+}

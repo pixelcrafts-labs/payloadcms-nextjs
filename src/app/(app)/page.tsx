4a1c0b40ac699c88375e04985ba210b894101234
vuquangpham <vuquangpham2909@gmail.com>
2024-06-27 20:16:44 +0700

    feat: adjust header and sample page

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,9 +1,14 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="mt-12 text-center mt-50">
-      <h1>Hello and welcome!</h1>
+      <h1 className="mb-10">Hello and welcome!</h1>
+      <Button variant={"outline"}>
+        <Link href={"/typography"}>Go to typography</Link>
+      </Button>
     </main>
   );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,17 +1,18 @@
 import Image from "next/image";
 import ColorModeSwitcher from "./color-mode-switcher";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <div className="w-full flex justify-between items-center px-5 min-h-[var(--header-height)] shadow-sm">
-      <div className="relative aspect-[300/68] w-20">
+      <Link href={"/"} className="relative inline-block aspect-[300/68] w-20">
         <Image
           src="/vercel.svg"
           alt="header-logo"
           fill
           className="object-contain dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
         />
-      </div>
+      </Link>
       <ColorModeSwitcher />
     </div>
   );

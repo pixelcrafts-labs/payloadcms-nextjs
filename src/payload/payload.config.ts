a4703032bb136842fe0f94fb2f977429d34568c6
vuquangpham <vuquangpham2909@gmail.com>
2024-07-03 03:28:26 +0700

    feat: add more comment and regenerate types

--- src/payload/payload.config.ts
+++ src/payload/payload.config.ts
@@ -20,17 +20,33 @@ export default buildConfig({
 	admin: {
 		user: Users.slug,
 	},
+
+	// Define and configure your collections in this array
 	collections: [Media, Pages, Users],
-	editor: lexicalEditor(),
-	secret: ENV.PAYLOAD_SECRET,
+
 	typescript: {
 		outputFile: path.resolve(dirname, "payload-types.ts"),
 	},
+
+	// If you'd like to use Rich Text, pass your editor here
+	editor: lexicalEditor(),
+
+	// Your Payload secret - should be a complex and secure string, unguessable
+	secret: ENV.PAYLOAD_SECRET,
+
+	// Whichever database adapter you're using should go here
+	// Mongoose is shown as an example, but you can also use Postgres
 	db: mongooseAdapter({
 		url: ENV.PAYLOAD_DATABASE_URI,
 	}),
+
+	// If you want to resize images, crop, set focal point, etc.
+	// make sure to install it and pass it to the config.
+	// This is optional - if you don't need to do these things,
+	// you don't need it!
 	sharp,
 
+	// Configuring email for the CMS
 	// email: emailConfiguration,
 
 	plugins: [

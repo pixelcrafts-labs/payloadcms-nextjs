91794ea4d49e9695cb3689b7c5d2de8429e3b33f
vuquangpham <vuquangpham2909@gmail.com>
2024-07-03 03:40:13 +0700

    feat: using React Compiler for the project

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ import { withPayload } from "@payloadcms/next/withPayload";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	// Your Next.js config here
+
+	// https://react.dev/learn/react-compiler#usage-with-nextjs
+	experimental: {
+		reactCompiler: true,
+	},
 };
 
 export default withPayload(nextConfig);

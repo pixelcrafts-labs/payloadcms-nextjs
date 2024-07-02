a1476cdd3b358ea36caa81ad71bfbd1a6e9fcdbf
vuquangpham <vuquangpham2909@gmail.com>
2024-07-03 03:19:43 +0700

    fix: type of access

--- src/payload/access/admin.ts
+++ src/payload/access/admin.ts
@@ -1,9 +1,7 @@
-import type { AccessArgs } from "payload";
+import type { FieldAccess } from "payload";
 import type { User } from "@cms/payload-types";
 import { checkRoles } from "@cms/collections/Users/utils/check-role";
 import { ROLES } from "../collections/Users/utils/roles";
 
-type isAdmin = (args: AccessArgs<User>) => boolean;
-
-export const admin: isAdmin = ({ req: { user } }) =>
+export const admin: FieldAccess = ({ req: { user } }) =>
 	checkRoles([ROLES.ADMIN as "admin"], user as User);

36647423e766aed965528449fce740ae77bc8d04
vuquangpham <vuquangpham2909@gmail.com>
2024-07-03 16:13:11 +0700

    feat: enable React Compiler in Next15

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ import { withPayload } from "@payloadcms/next/withPayload";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	// Your Next.js config here
+	experimental: {
+		reactCompiler: true,
+	},
 };
 
 export default withPayload(nextConfig);

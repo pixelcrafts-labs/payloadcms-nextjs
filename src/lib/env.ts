07b1ee808bd6d82478ecb9ed9977b7359c5853b5
vuquangpham <vuquangpham2909@gmail.com>
2024-07-09 21:46:40 +0700

    chore: make the payload email optional

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -6,12 +6,12 @@ const envSchema = z.object({
 	PAYLOAD_CONFIG_PATH: z.string(),
 
 	// Email
-	PAYLOAD_EMAIL_ADDRESS: z.string(),
-	PAYLOAD_EMAIL_NAME: z.string(),
-	PAYLOAD_SMTP_HOST: z.string(),
-	PAYLOAD_SMTP_PORT: z.coerce.number(),
-	PAYLOAD_SMTP_USER: z.string(),
-	PAYLOAD_SMTP_PASS: z.string(),
+	PAYLOAD_EMAIL_ADDRESS: z.string().optional(),
+	PAYLOAD_EMAIL_NAME: z.string().optional(),
+	PAYLOAD_SMTP_HOST: z.string().optional(),
+	PAYLOAD_SMTP_PORT: z.coerce.number().optional(),
+	PAYLOAD_SMTP_USER: z.string().optional(),
+	PAYLOAD_SMTP_PASS: z.string().optional(),
 });
 
 const ENV = envSchema.parse(process.env);

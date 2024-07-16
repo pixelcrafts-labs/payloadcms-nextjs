b0b7f0afc50539309e4f2b1b64340c67f94ab6ca
vuquangpham <vuquangpham2909@gmail.com>
2024-07-17 00:47:36 +0700

    feat: remove redundant page

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,13 +1,7 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-
 export default function Home() {
 	return (
 		<main className="mt-12 text-center mt-50">
 			<h1 className="mb-10">Hello and welcome!</h1>
-			<Button variant={"outline"}>
-				<Link href={"/typography"}>Go to typography</Link>
-			</Button>
 		</main>
 	);
 }

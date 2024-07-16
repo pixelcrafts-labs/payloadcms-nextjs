8d697f36d64ecc77f771f7a1d30cfd26cbb2a4ec
vuquangpham <vuquangpham2909@gmail.com>
2024-07-16 18:45:30 +0700

    refactor: uncomment the email configuration

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -5,13 +5,13 @@ const envSchema = z.object({
 	PAYLOAD_SECRET: z.string(),
 	PAYLOAD_CONFIG_PATH: z.string(),
 
-	// // Email
-	// PAYLOAD_EMAIL_ADDRESS: z.string(),
-	// PAYLOAD_EMAIL_NAME: z.string(),
-	// PAYLOAD_SMTP_HOST: z.string(),
-	// PAYLOAD_SMTP_PORT: z.coerce.number(),
-	// PAYLOAD_SMTP_USER: z.string(),
-	// PAYLOAD_SMTP_PASS: z.string(),
+	// Email
+	PAYLOAD_EMAIL_ADDRESS: z.string(),
+	PAYLOAD_EMAIL_NAME: z.string(),
+	PAYLOAD_SMTP_HOST: z.string(),
+	PAYLOAD_SMTP_PORT: z.coerce.number(),
+	PAYLOAD_SMTP_USER: z.string(),
+	PAYLOAD_SMTP_PASS: z.string(),
 });
 
 const ENV = envSchema.parse(process.env);

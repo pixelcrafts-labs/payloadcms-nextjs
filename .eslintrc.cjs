805e7fabe29d22b24c11a540ba149e5725019162
vuquangpham <vuquangpham2909@gmail.com>
2024-07-22 01:24:08 +0700

    feat: remove redundant eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,4 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
 	extends: ["next/core-web-vitals", "prettier"],
-	parserOptions: {
-		project: ["./tsconfig.json"],
-		tsconfigRootDir: __dirname,
-	},
-
-	// React 19
-	// https://react.dev/learn/react-compiler#installing-eslint-plugin-react-compiler
-	plugins: ["eslint-plugin-react-compiler"],
-	rules: {
-		"react-compiler/react-compiler": "error",
-	},
 };

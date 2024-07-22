e7567fe5920845b5e74652192237b8985600cd80
vuquangpham <vuquangpham2909@gmail.com>
2024-07-22 16:08:04 +0700

    refactor: remove tailwindcss config

--- src/app/(payload)/layout.tsx
+++ src/app/(payload)/layout.tsx
@@ -5,7 +5,6 @@ import { RootLayout } from "@payloadcms/next/layouts";
 /* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
 import React from "react";
 
-import "@/styles/globals.css";
 import "./custom.scss";
 
 type Args = {

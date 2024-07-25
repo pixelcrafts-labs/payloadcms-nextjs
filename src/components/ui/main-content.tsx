12e50575da02214c88b6d17ea093800d493ed580
vuquangpham <vuquangpham2909@gmail.com>
2024-07-25 12:15:57 +0700

    refactor: change to semantic tag

--- src/components/ui/main-content.tsx
+++ src/components/ui/main-content.tsx
@@ -1,5 +1,5 @@
 import type { ReactNode } from "react";
 
 export default function MainContent({ children }: { children: ReactNode }) {
-	return <div className="pt-header-height">{children}</div>;
+	return <main className="pt-header-height">{children}</main>;
 }

4fd79398fefae27a804f63f906ed97e44f45e772
vuquangpham <vuquangpham2909@gmail.com>
2024-08-13 17:32:29 +0700

    refactor: refactor payload components

--- src/components/payload-components/index.tsx
+++ src/components/payload-components/index.tsx
@@ -1,7 +1,7 @@
 import React, { Fragment } from "react";
+import type { Page } from "@/payload/payload-types";
 import Content from "./content";
 import ImageContent from "./image-content";
-import type { Page } from "@/payload/payload-types";
 
 type Props = {
 	components: Page["components"];
@@ -23,7 +23,7 @@ export default function PayloadComponents({ components }: Props) {
 		<>
 			{components
 				?.map((c) => {
-					const { blockType } = c;
+					const { blockType, options } = c;
 					const data = (c as any)[blockType];
 
 					// find corresponding component
@@ -35,7 +35,11 @@ export default function PayloadComponents({ components }: Props) {
 					if (!result) return null;
 
 					const { component } = result;
-					return <Fragment key={blockType}>{component(data)}</Fragment>;
+					return (
+						<Fragment key={blockType}>
+							{component({ ...data, options })}
+						</Fragment>
+					);
 				})
 				.filter(Boolean)}
 		</>

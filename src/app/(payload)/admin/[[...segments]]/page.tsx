dc40031f3ecaa48bb154bc486019ca4cc16df6b6
vuquangpham <vuquangpham2909@gmail.com>
2024-08-18 18:33:15 +0700

    feat: upgrade payloadcms

--- src/app/(payload)/admin/[[...segments]]/page.tsx
+++ src/app/(payload)/admin/[[...segments]]/page.tsx
@@ -1,26 +1,24 @@
 /* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
-import type { Metadata } from "next";
+import type { Metadata } from 'next'
 
-import config from "@/payload/payload.config";
+import config from '@payload-config'
 /* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
-import { RootPage, generatePageMetadata } from "@payloadcms/next/views";
+import { RootPage, generatePageMetadata } from '@payloadcms/next/views'
+import { importMap } from '../importMap'
 
 type Args = {
-	params: {
-		segments: string[];
-	};
-	searchParams: {
-		[key: string]: string | string[];
-	};
-};
+  params: {
+    segments: string[]
+  }
+  searchParams: {
+    [key: string]: string | string[]
+  }
+}
 
-export const generateMetadata = ({
-	params,
-	searchParams,
-}: Args): Promise<Metadata> =>
-	generatePageMetadata({ config, params, searchParams });
+export const generateMetadata = ({ params, searchParams }: Args): Promise<Metadata> =>
+  generatePageMetadata({ config, params, searchParams })
 
 const Page = ({ params, searchParams }: Args) =>
-	RootPage({ config, params, searchParams });
+  RootPage({ config, params, searchParams, importMap })
 
-export default Page;
+export default Page
---
+++ src/app/(payload)/admin/importMap.js
@@ -0,0 +1,33 @@
+import { RichTextCell as RichTextCell_0 } from '@payloadcms/richtext-lexical/client'
+import { RichTextField as RichTextField_1 } from '@payloadcms/richtext-lexical/client'
+import { getGenerateComponentMap as getGenerateComponentMap_2 } from '@payloadcms/richtext-lexical/generateComponentMap'
+import { LinkFeatureClient as LinkFeatureClient_3 } from '@payloadcms/richtext-lexical/client'
+import { HorizontalRuleFeatureClient as HorizontalRuleFeatureClient_4 } from '@payloadcms/richtext-lexical/client'
+import { InlineToolbarFeatureClient as InlineToolbarFeatureClient_5 } from '@payloadcms/richtext-lexical/client'
+import { OrderedListFeatureClient as OrderedListFeatureClient_6 } from '@payloadcms/richtext-lexical/client'
+import { UnorderedListFeatureClient as UnorderedListFeatureClient_7 } from '@payloadcms/richtext-lexical/client'
+import { AlignFeatureClient as AlignFeatureClient_8 } from '@payloadcms/richtext-lexical/client'
+import { UnderlineFeatureClient as UnderlineFeatureClient_9 } from '@payloadcms/richtext-lexical/client'
+import { BoldFeatureClient as BoldFeatureClient_10 } from '@payloadcms/richtext-lexical/client'
+import { ItalicFeatureClient as ItalicFeatureClient_11 } from '@payloadcms/richtext-lexical/client'
+import { BlockquoteFeatureClient as BlockquoteFeatureClient_12 } from '@payloadcms/richtext-lexical/client'
+import { HeadingFeatureClient as HeadingFeatureClient_13 } from '@payloadcms/richtext-lexical/client'
+import { ParagraphFeatureClient as ParagraphFeatureClient_14 } from '@payloadcms/richtext-lexical/client'
+
+export const importMap = {
+  "@payloadcms/richtext-lexical/client#RichTextCell": RichTextCell_0,
+  "@payloadcms/richtext-lexical/client#RichTextField": RichTextField_1,
+  "@payloadcms/richtext-lexical/generateComponentMap#getGenerateComponentMap": getGenerateComponentMap_2,
+  "@payloadcms/richtext-lexical/client#LinkFeatureClient": LinkFeatureClient_3,
+  "@payloadcms/richtext-lexical/client#HorizontalRuleFeatureClient": HorizontalRuleFeatureClient_4,
+  "@payloadcms/richtext-lexical/client#InlineToolbarFeatureClient": InlineToolbarFeatureClient_5,
+  "@payloadcms/richtext-lexical/client#OrderedListFeatureClient": OrderedListFeatureClient_6,
+  "@payloadcms/richtext-lexical/client#UnorderedListFeatureClient": UnorderedListFeatureClient_7,
+  "@payloadcms/richtext-lexical/client#AlignFeatureClient": AlignFeatureClient_8,
+  "@payloadcms/richtext-lexical/client#UnderlineFeatureClient": UnderlineFeatureClient_9,
+  "@payloadcms/richtext-lexical/client#BoldFeatureClient": BoldFeatureClient_10,
+  "@payloadcms/richtext-lexical/client#ItalicFeatureClient": ItalicFeatureClient_11,
+  "@payloadcms/richtext-lexical/client#BlockquoteFeatureClient": BlockquoteFeatureClient_12,
+  "@payloadcms/richtext-lexical/client#HeadingFeatureClient": HeadingFeatureClient_13,
+  "@payloadcms/richtext-lexical/client#ParagraphFeatureClient": ParagraphFeatureClient_14
+}

0326780bf97fa9a09c8f37570d09edc5cb9c5af6
vuquangpham <vuquangpham2909@gmail.com>
2024-08-19 21:01:09 +0700

    feat: turn off hmr in production

--- src/lib/payload.ts
+++ src/lib/payload.ts
@@ -1,7 +1,10 @@
 import { getPayloadHMR } from "@payloadcms/next/utilities";
+import { getPayload as getPayLoadProd } from "payload";
 import configPromise from "@payload-config";
 
 // for getting the latest instance
 export default function getPayload() {
-	return getPayloadHMR({ config: configPromise });
+	const fn =
+		process.env["NODE_ENV"] === "development" ? getPayloadHMR : getPayLoadProd;
+	return fn({ config: configPromise });
 }
